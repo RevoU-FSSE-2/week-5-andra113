1bded9aa0e1e77937721f8ff6abcc3cc8cb225d4
Andra <andradwikasa@gmail.com>
2023-07-12 13:05:13 +0700

    added more context to services

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const clickKonsultasi = document.getElementById('konsultasi')
-const showHiddenKonsultasi = document.getElementById('hidden-shown')
+const clickMentoring = document.getElementById('mentoring')
+const clickTes = document.getElementById('tes')
+
+const showHiddenKonsultasi = document.getElementById('hidden-shown konsultasi')
+const showHiddenMentoring = document.getElementById('hidden-shown mentoring')
+const showHiddenTes = document.getElementById('hidden-shown tes')
 
 clickKonsultasi.addEventListener('click', function() {
     if (showHiddenKonsultasi.style.display === 'none') {
         showHiddenKonsultasi.style.display = 'flex'
+        showHiddenMentoring.style.display ='none'
+        showHiddenTes.style.display = 'none'
     } else {
         showHiddenKonsultasi.style.display = 'none'
+    }
+})
+
+clickMentoring.addEventListener('click', function() {
+    if (showHiddenMentoring.style.display === 'none') {
+        showHiddenMentoring.style.display = 'flex'
+        showHiddenKonsultasi.style.display ='none'
+        showHiddenTes.style.display = 'none'
+    } else {
+        showHiddenMentoring.style.display = 'none'
+    }
+})
+
+clickTes.addEventListener('click', function() {
+    if (showHiddenTes.style.display === 'none') {
+        showHiddenTes.style.display = 'flex'
+        showHiddenMentoring.style.display ='none'
+        showHiddenKonsultasi.style.display = 'none'
+    } else {
+        showHiddenTes.style.display = 'none'
     }
 })
